34c72715e58a4a2601f742b7a515331419cb85b8
Horea Constandache <fortech.constandache@extaccount.com>
2019-05-28 13:39:52 +0300

    connection with sql db

--- commons/utils.py
+++ commons/utils.py
@@ -1,8 +1,25 @@
 from commons.constants import TODOS
 from flask_restful import abort
+from flask import jsonify
 
 
 def abort_if_todo_doesnt_exist(todo_id):
 
     if todo_id not in TODOS:
         abort(404, message="Todo {} doesn't exist".format(todo_id))
+
+
+def format_book_list(cur):
+    books = []
+    for row in cur:
+        books.append({
+            'ISBN': row[0],
+            'Title': row[1],
+            'Author': row[2],
+            'Description': row[3],
+            'PublishDate': row[4],
+            'Publisher': row[5],
+            'NrOfCopies': row[6],
+            'NrOfCopiesAvailable': row[7]
+        })
+    return jsonify(books)
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from flask_restful import Api
 from resources.todo import Todo
-from resources.todolist import ToDoList
+from resources.booksList import BookList
 
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(ToDoList, '/todos')
+api.add_resource(BookList, '/getBooks')
 api.add_resource(Todo, '/todos/<todo_id>')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
